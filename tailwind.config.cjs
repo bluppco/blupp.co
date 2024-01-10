ba66e2c86183289edad6090113098cb58d35fd96
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-10 20:50:22 +0530

    x

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+			colors: {
+
+				blupp_black: "#030303",
+				blupp_gray: "#A3A3A3"
+
+			}
+
+		},
 	},
 	plugins: [],
 }
