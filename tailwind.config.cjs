f5f025230fb1fe5ab373f19a3b5343b305a1edc1
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-13 01:18:36 +0530

    enhancement: updated styling in index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,30 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+
+		screens: {
+
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'mac': '1440px',
+			'2xl': '1536px'
+
+		},
+
 		extend: {
+
+			animation: {
+				fade: 'fadeOut 2s ease-in-out',
+			},
+
+			keyframes: theme => ({
+				fadeOut: {
+					'0%': { color: theme('colors.transparent') },
+					'100%': { color: theme('colors.slate.900') },
+				},
+			  }),
 
 			fontFamily: {
 
