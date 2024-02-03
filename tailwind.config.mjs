d9fa97ca2b6db740652a7700c56a336fc7c08f23
Mohit <mohit@hey.com>
2024-02-04 00:05:13 +0530

    x

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,9 +10,28 @@ module.exports = {
 			'mac': '1440px',
 
 		},
-
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
-
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 			animation: {
 				fade: 'fadeOut 2s ease-in-out',
 			},
@@ -39,30 +58,6 @@ module.exports = {
 				blupp_gray: "#A3A3A3"
 
 			}
-
-		},
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
-		extend: {
-			keyframes: {
-				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
