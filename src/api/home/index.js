1bc1e7601da19ed84b5946af5a17391a7b79debb
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-07 10:42:57 +0530

    feat: made index page dynamic via API

---
+++ src/api/home/index.js
@@ -0,0 +1,41 @@
+export const homeAPI = async () => {
+
+    const endpoint = "https://staging.api.blupp.co"
+
+    // NETWORK CALL TO GET DATA
+    const network_data = await fetch( endpoint + "/home" )
+    const data_json = await network_data.json()
+
+    const { home } = data_json.data
+
+    let home_data = {
+
+        // HOME PROPS
+        hero : "",
+        how_we_work : "",
+        tech_stack : "",
+        clients : "",
+        projects : "",
+        blogs : "",
+
+    }
+    home.map( ( data ) => {
+
+        if( data.slug === "hero" )
+            home_data.hero = data
+        else if( data.slug === "how-we-work" )
+            home_data.how_we_work = data
+        else if( data.slug === "tech-stack" )
+            home_data.tech_stack = data
+        else if( data.slug === "clients" )
+            home_data.clients = data
+        else if( data.slug === "projects" )
+            home_data.projects = data
+        else if( data.slug === "blogs" )
+            home_data.blogs = data
+
+    })
+
+    return { home_data }
+
+}
