d10a1b2c33c2a0144b644955f1169a497ed89f98
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-07 14:50:06 +0530

    chores: installed cobe package

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -45,7 +45,7 @@ const Header = ( props ) => {
 
     return(
         <>
-            <header class="hidden md:block">
+            <header className="hidden md:block">
                 <motion.header className="flex items-center h-20 w-full fixed z-50 bg-gray-100"
                     variants={ squareVariants }
                     initial="display"
@@ -53,15 +53,15 @@ const Header = ( props ) => {
                     onMouseLeave={ () => updateNavigationDisplay( null ) }
                 >
                     <Container>
-                        <section class="flex justify-between items-center h-full">
+                        <section className="flex justify-between items-center h-full">
                             <Link href="/" aria_label="">
                                 <div class="flex items-center gap-2">
-                                    <p class="text-2xl text-white bg-black rounded-full w-12 aspect-square text-center flex justify-center items-center">&#120121;</p>
-                                    <p class="text-xl font-inter_tight tracking-wide">Blupp Company</p>
+                                    <p className="text-2xl text-white bg-black rounded-full w-12 aspect-square text-center flex justify-center items-center">&#120121;</p>
+                                    <p className="text-xl font-inter_tight tracking-wide">Blupp Company</p>
                                 </div>
                             </Link>
-                            <nav class="flex items-center h-full justify-end">
-                                <ul class="flex items-center gap-8">
+                            <nav className="flex items-center h-full justify-end">
+                                <ul className="flex items-center gap-8">
                                     <Li href="/about">Company</Li>
                                     <Li href="/projects">Projects</Li>
                                     <Li href="/blog">Blogs</Li>
