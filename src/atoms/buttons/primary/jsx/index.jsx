7e35a23dcbbf7dc187bb65ca6a50edcc149b98d6
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-02-08 16:19:01 +0530

    x

--- src/atoms/buttons/primary/jsx/index.jsx
+++ src/atoms/buttons/primary/jsx/index.jsx
@@ -1,7 +1,7 @@
 const PrimaryButton = ( props ) => {
 
     return(
-        <button class="bg-black/70 hover:bg-black transition-all duration-300 h-10 flex items-center px-4 rounded-lg text-white text-sm tracking-widest font-inter_tight border border-gray-200">
+        <button className="bg-black/70 hover:bg-black transition-all duration-300 h-10 flex items-center px-4 rounded-lg text-white text-sm tracking-widest font-inter_tight border border-gray-200">
             { props.children }
         </button>
 
